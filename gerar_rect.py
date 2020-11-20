c30d2bb01548f027f47bfd13f109182db8b9fa2b
Rafael Beserra <rafaelufrn@gmail.com>
2020-11-20 10:24:48 -0300

    Add files via upload

--- gerar_rect.py
+++ gerar_rect.py
@@ -14,14 +14,23 @@ fixedSize = 'size' in kwargs
 fixedTheta = 'angle' in kwargs
 #se vai utilizar um numero de quadrados fixo
 fixedNumber = 'number' in kwargs
+#se vai testar sobreposicao
+overlapTest = 'overlapTest' in kwargs
 
 if fixedNumber:
 	n = int(kwargs['number'])
 else:
 	n = random.randint(3, 100)
 
+def side(p0, p1, q):
+	v0 = (p1[0] - p0[0], p1[1] - p0[1])
+	v1 = (q[0]  - p0[0], q[1]  - p0[1])
+	return v0[0]*v1[1] - v0[1]*v1[0]
+
 print(n)
-for k in range(n):
+rects = []
+k = 0
+while k < n:
 	if fixedSize:
 		size = int(kwargs['size'])
 	else:
@@ -35,11 +44,26 @@ for k in range(n):
 	theta = angle + math.pi/4
 	y = random.randint(int(size), H-1-int(size))
 	x = random.randint(int(size), W-1-int(size))
-	print(str(x) + ' ' + str(y) + ' ' + str(size) + ' ' + str(angle))
 	pts = []
 	for i in range(4):
 		pts.append((x+hd*math.cos(theta+i*math.pi/2), y-hd*math.sin(theta+i*math.pi/2)))
-	cv2.fillPoly(img, np.int32([pts]), 255)
+	s = (pts[0], pts[1], pts[2], pts[3])
+	overlap = False
+	if overlapTest:
+		for r in rects:
+			for i in range(4):
+				for j in range(4):
+					v0 = (s[i], s[(i+1)%4])
+					v1 = (r[j], r[(j+1)%4])
+					side0 = side(v0[0], v0[1], v1[0])*side(v0[0], v0[1], v1[1])
+					side1 = side(v1[0], v1[1], v0[0])*side(v1[0], v1[1], v0[1])
+					if side0 < 0 and side1 < 0:
+						overlap = True
+	if not overlap:
+		k += 1
+		print(str(x) + ' ' + str(y) + ' ' + str(size) + ' ' + str(angle))
+		rects.append((pts[0], pts[1], pts[2], pts[3]))
+		cv2.fillPoly(img, np.int32([pts]), 255)
 
 cv2.imshow('janela', img.astype(np.uint8))
 cv2.imwrite(sys.argv[1], img)
